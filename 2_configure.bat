37644ce637c7b0ceb21d22fc1d3b9c76bd8243d1
Capric <capric8416@gmail.com>
2024-08-26 10:56:20 +0800

    [refactor] move files in sub test_qt_libmpv dir out

--- 2_configure.bat
+++ 2_configure.bat
@@ -5,7 +5,7 @@
 cmake                                                                    ^
     -S .                                                                 ^
     -B project/build                                                     ^
-    -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake     ^
+    -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake      ^
     -DVCPKG_TARGET_TRIPLET=x64-windows-static                            ^
     -DVCPKG_HOST_TRIPLET=x64-windows-static
 
